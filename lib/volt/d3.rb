a0ea82ea5688f8e708ecd7f15be8aea3806d7038
Robert H Grayson II <bobbygrayson@gmail.com>
2015-04-05 22:41:48 -0400

    add some basic ruby bindings.

--- lib/volt/d3.rb
+++ lib/volt/d3.rb
@@ -1,5 +1,13 @@
 module Volt
   class D3
-    # Your code goes here...
+    def event
+      # This global is registered during an event listener callback with the on operator.
+      `d3.event`
+    end
+
+    def mouse container
+      # returns the x, y coordinates of the mouse position in the current event
+      `d3.mouse`
+    end
   end
 end
