ba7122528bfdbd17a368fd4f3de50337a8e02d46
Robert H Grayson II <bobbygrayson@gmail.com>
2015-04-05 22:30:42 -0400

    add example application

---
+++ example/app/main/config/dependencies.rb
@@ -0,0 +1,13 @@
+# Specify which components you wish to include when
+# the "home" component loads.
+
+# bootstrap css framework
+component 'bootstrap'
+
+# a default theme for the bootstrap framework
+component 'bootstrap-jumbotron-theme'
+
+# provides templates for login, signup, and logout
+component 'user-templates'
+
+component 'd3'
